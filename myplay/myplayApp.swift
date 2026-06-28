b3092073069d24e5797fe98e4106567662e26614
ceif <hugboss@hotmail.com>
2026-06-28 19:42:30 +0100

    Add files via upload

---
+++ myplay/myplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  myplayApp.swift
+//  myplay
+//
+//  Created by Hugo Fonseca on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct myplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
